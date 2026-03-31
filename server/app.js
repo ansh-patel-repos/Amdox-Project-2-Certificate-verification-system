aec2a4dd05edece7177b89fe6c1c726d9c384a99
ansh46033512 <patelansh6989@gmail.com>
2026-03-31 14:43:49 +0530

    Final changes

--- server/app.js
+++ server/app.js
@@ -8,7 +8,14 @@ dotenv.config();
 connectDB();
 const app = express();
 
-app.use(express.json());
+app.use(
+  express.json({
+    verify: (req, res, buf) => {
+      req.rawBody = buf.toString();
+    },
+  })
+);
+
 app.get("/", (req, res) => {
     res.send("Backend is running");
 })
--- server/controllers/webhooks.js
+++ server/controllers/webhooks.js
@@ -7,7 +7,7 @@ export const clerkWebhooks = async (req, res) => {
     // create a webhook svix instance
     const webhook = new Webhook(process.env.CLERK_WEBHOOK_SECRET);
 
-    await webhook.verify(JSON.stringify(req.body), {
+    await webhook.verify(req.rawBody, {
       "svix-id": req.headers["svix-id"],
       "svix-timestamp": req.headers["svix-timestamp"],
       "svix-signature": req.headers["svix-signature"],
