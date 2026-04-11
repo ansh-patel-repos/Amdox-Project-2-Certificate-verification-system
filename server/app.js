813ee932f14f4dd119a7aaecd736034d4f6411c5
ansh46033512 <patelansh6989@gmail.com>
2026-04-12 01:06:25 +0530

    Final commit

--- server/app.js
+++ server/app.js
@@ -13,7 +13,7 @@ import { clerkMiddleware } from "@clerk/express";
 connectDB();
 const app = express();
 
-app.use(cors())
+app.use(cors({origin: "*"}))
 app.use(express.json())
 app.use(clerkMiddleware());
 
